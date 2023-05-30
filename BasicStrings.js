db294cb45cafbcc325983e4d7985c91600bb4116
Anderson Kmetiuk <anderson.l.kmetiuk@exxonmobil.com>
2023-05-30 10:33:09 -0300

    Create BasicStrings.js

---
+++ BasicStrings.js
@@ -0,0 +1,15 @@
+var myString = "This is my string";
+
+//Method that converts strings to uppercase
+console.log(myString.toUpperCase());
+
+//Return string size
+console.log(myString.length);
+
+//Shows variable value
+console.log(`This is the size of the string: ${myString.length}`);
+
+//Note: the syntax needs ` `. Let`s see the differences
+console.log("This is the size of the string: ${myString.length}");
+console.log("This is the size of the string: ${myString.length}");
+console.log(`This is the size of the string: "${myString.length}"`);
