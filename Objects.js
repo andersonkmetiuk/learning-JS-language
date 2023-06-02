c5f62df482b3a9de3fafe7eb3cef6bf2cf8f8c59
Anderson Kmetiuk <anderson.l.kmetiuk@exxonmobil.com>
2023-06-02 08:41:18 -0300

    updated object with references

--- Objects.js
+++ Objects.js
@@ -55,3 +55,31 @@ console.log(bird.color);
 console.log(bird["where it lives"]);
 
 delete bird.color; //deles a property
+
+/*References
+When you type */
+console.log("\nreferences");
+let bird2 = bird;
+/*you are creating a reference pointing to the same location in memory
+if you try to change a value in bird2 it will also change in the original
+let`s see*/
+bird2.genus = "ursus";
+console.log("Bird (Reference)");
+console.log(bird);
+
+bird.genus = "corvus";
+console.log("Bird2 (Reference)");
+console.log(bird2);
+
+// to create a real copy you need to use this instead
+bird2 = Object.assign({}, bird);
+//with the spread operator as well (...)
+bird2 = { ...bird };
+/*both line 72 and 74 are equal
+finally another way is this one:
+JSON = JavaScript Object Notation */
+bird2 = JSON.parse(JSON.stringify(bird));
+
+bird2.genus = "ursus";
+console.log("Bird (Copy)");
+console.log(bird);
