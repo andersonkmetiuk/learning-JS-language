affd88022e2bce978e5d990153c2bafb1d39afef
Anderson Kmetiuk <anderson.l.kmetiuk@exxonmobil.com>
2023-06-07 19:45:31 -0300

    Create conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,8 @@
+//if
+var answer = window.confirm("Click OK, get true. Click cancel, get false.");
+
+if (answer === true) {
+  console.log("You said true!");
+} else {
+  console.log("You said false!");
+}
