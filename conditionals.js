0221432e5351fdca6651597439e987ba9af874b4
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-06-07 20:21:02 -0300

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,8 +1,30 @@
-//if
-var answer = window.confirm("Click OK, get true. Click cancel, get false.");
+//examples of conditionals
 
+//if
 if (answer === true) {
   console.log("You said true!");
 } else {
   console.log("You said false!");
+}
+
+//else if
+if (answer === "YES") {
+  console.log("Yes!");
+} else if (answer === "NO") {
+  console.log("No!");
+} else {
+  console.log("Okay then");
+}
+
+//switch case
+switch (answer) {
+  case "YES":
+    console.log("Yes!");
+    break;
+  case "NO":
+    console.log("No!");
+    break;
+  default:
+    console.log("Okay then");
+    break;
 }
